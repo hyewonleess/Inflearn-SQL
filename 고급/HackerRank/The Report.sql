aef728781fa52a50585cbf1f13e14bc507dd89da
HYEWON LEE <hyewon328@naver.com>
2022-04-10 17:09:01 +0900

    Create The Report.sql

---
+++ 고급/HackerRank/The Report.sql
@@ -0,0 +1,4 @@
+SELECT CASE WHEN g.grade < 8 THEN NULL ELSE s.name END AS name, g.grade, s.marks
+FROM students s
+INNER JOIN grades g ON s.marks BETWEEN g.min_mark AND g.max_mark
+ORDER BY g.grade DESC, name
